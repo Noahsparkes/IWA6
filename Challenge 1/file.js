acf31f8b76ac070762192a419ab312b1ee395e37
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-04 23:25:53 +0200

    added isNaN() to check if string

--- Challenge 1/file.js
+++ Challenge 1/file.js
@@ -3,8 +3,8 @@ const secondaryPhone = '0219131568';
 
 // Only change below this line
 
-const primaryValid = typeof number === primaryPhone 
-const secondaryValid = typeof number === secondaryPhone
+const primaryValid = !isNaN(primaryPhone) && typeof primaryPhone  === 'string';
+const secondaryValid = !isNaN(primaryPhone) && typeof secondaryPhone === 'string';
 
-console.log('Primary phone is valid numerical string:', primaryValid)
-console.log('Secondary phone is valid numerical string:', secondaryValid )
+console.log('Primary phone is valid numerical string:', primaryValid);
+console.log('Secondary phone is valid numerical string:', secondaryValid );
